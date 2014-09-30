60c3f6b9ab1c4dc02d93374a2fa6a6373cab9dd2
Wallouf <ct13860@gmail.com>
2014-09-30 13:53:52 -0400

    Update with static

--- src/com/wallouf/servlets/Test.java
+++ src/com/wallouf/servlets/Test.java
@@ -1,6 +1,5 @@
 package com.wallouf.servlets;
 
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,54 +13,51 @@ import org.joda.time.DateTime;
 
 import com.wallouf.beans.Coyote;
 
-
 public class Test extends HttpServlet {
-	/**
-	 * Static variables
-	 */
-	public static final String ATT_MESSAGE = "texte";
-	public static final String ATT_BEAN = "coyote";
-	public static final String ATT_LISTE = "liste";
-	public static final String ATT_JOUR = "jour";
-	public static final String VUE = "/WEB-INF/test.jsp";
-	
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-		String testText = new String("Miaouh");
-		
-		
-		Coyote premierBean = new Coyote();
-		premierBean.setNom("robert");
-		premierBean.setPrenom("de niro");
-		
-		/**
-		 * Creation dune liste
-		 */
-		List<Integer> premiereListe = new ArrayList<Integer>();
-		premiereListe.add(27);
-		premiereListe.add(12);
-		premiereListe.add(123);
-		premiereListe.add(8);
-		
-		/**
-		 * Date avec la librairie Joda
-		 */
-		
-		DateTime dt = new DateTime();
-		Integer jourDuMois = dt.getDayOfMonth();
-		
-		/**
-		 * Passe les attributs
-		 */
+    /**
+     * Static variables
+     */
+    public static final String ATT_MESSAGE = "texte";
+    public static final String ATT_BEAN    = "coyote";
+    public static final String ATT_LISTE   = "liste";
+    public static final String ATT_JOUR    = "jour";
+    public static final String VUE         = "/WEB-INF/test.jsp";
 
-		request.setAttribute(ATT_MESSAGE, testText);
-		request.setAttribute(ATT_BEAN, premierBean);
-		request.setAttribute(ATT_LISTE, premiereListe);
-		request.setAttribute(ATT_JOUR, jourDuMois);
-		
-		
-		/**		
-		 * envoi des donnees
-		 */
-		this.getServletContext().getRequestDispatcher(VUE).forward(request, response);
-	}
+    public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
+        String testText = new String( "Miaouh" );
+
+        Coyote premierBean = new Coyote();
+        premierBean.setNom( "robert" );
+        premierBean.setPrenom( "de niro" );
+
+        /**
+         * Creation dune liste
+         */
+        List<Integer> premiereListe = new ArrayList<Integer>();
+        premiereListe.add( 27 );
+        premiereListe.add( 12 );
+        premiereListe.add( 123 );
+        premiereListe.add( 8 );
+
+        /**
+         * Date avec la librairie Joda
+         */
+
+        DateTime dt = new DateTime();
+        Integer jourDuMois = dt.getDayOfMonth();
+
+        /**
+         * Passe les attributs
+         */
+
+        request.setAttribute( ATT_MESSAGE, testText );
+        request.setAttribute( ATT_BEAN, premierBean );
+        request.setAttribute( ATT_LISTE, premiereListe );
+        request.setAttribute( ATT_JOUR, jourDuMois );
+
+        /**
+         * envoi des donnees
+         */
+        this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
+    }
 }
